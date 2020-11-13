0047c467d04a57ea3240a2e18d3b2eb6d5ed187b
hongchen <744882174@qq.com>
2020-11-13 16:47:23 +0800

    eureka服务部署

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/Order/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/Order/OrderMain80.java
@@ -2,12 +2,14 @@ package com.atguigu.springcloud.Order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author HongChen
  * @date 2020/11/4 10:34
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
