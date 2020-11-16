b718f69ea34404bb6dd2cb627f06f47997db8e01
hongchen <744882174@qq.com>
2020-11-16 16:46:25 +0800

    123

---
+++ cloud-eureka-server7002/src/main/java/com/atguigu/springcloud/EurekaMain7002.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author HongChen
+ * @date 2020/11/13 15:14
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
