26d8ceea0351b9c939399cc7a79ecc2470b97752
hongchen <744882174@qq.com>
2020-11-17 18:05:55 +0800

    更新12

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/paymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/paymentController.java
@@ -4,6 +4,7 @@ import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -17,12 +18,14 @@ import javax.annotation.Resource;
 public class paymentController {
     @Resource
     private PaymentService paymentService;
+    @Value("${server.port}")
+    private String serverPort;
     @PostMapping("/payment/create")
     public CommonResult create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         log.info("插入结果:"+result);
         if(result>0){
-            return  new CommonResult(200,"插入数据成功",result);
+            return  new CommonResult(200,"插入数据成功,serverPort："+serverPort,result);
         }
         else{
             return new CommonResult(444,"插入数据库失败",null);
@@ -33,7 +36,7 @@ public class paymentController {
         Payment result = paymentService.getPaymentById(id);
         log.info("查询结果:"+result);
         if(result!=null){
-            return  new CommonResult(200,"查询数据成功",result);
+            return  new CommonResult(200,"查询数据成功,serverPort："+serverPort,result);
         }
         else{
             return new CommonResult(444,"没有对应记录，查询ID:"+id,null);
