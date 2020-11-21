1509d971a49d47dc0b747f96d9f0a054f4feb6d6
hongchen <744882174@qq.com>
2020-11-21 15:00:05 +0800

    eruka 服务结束

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/Order/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/Order/config/ApplicationContextConfig.java
@@ -1,6 +1,7 @@
 package com.atguigu.springcloud.Order.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ApplicationContextConfig {
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
